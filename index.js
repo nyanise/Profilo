124fb113409223fb8115d80b42cdc22ec8ffbff1
nyan <nyannajat8@gmail.com>
2018-09-18 02:25:56 +0300

    creating profilo

---
+++ index.js
@@ -0,0 +1,22 @@
+function pro(){
+    
+    $('.img1').on('click',function(){
+        $('.p1').css('display','block')
+    })
+        
+    $('.img2').on('click',function(){
+        $('.p2').css('display','block')})
+            
+    $('.img3').on('click',function(){
+        $('.p3').css('display','block')})
+            
+    $('.img4').on('click',function(){
+        $('.p4').css('display','block')})
+            
+    $('.img5').on('click',function(){
+        $('.p5').css('display','block')})
+            
+    $('.img6').on('click',function(){
+        $('.p6').css('display','block')})
+   
+}pro();
